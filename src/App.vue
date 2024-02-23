e5e091d390cc7ebf8cbd551dd53746232921eb6f
renzozuk <renzo.zuk19@gmail.com>
2024-02-22 22:30:41 -0300

    Project started.

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from "./components/learning-resources/StoredResources.vue";
+
+export default {
+    components: {
+        StoredResources,
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 1,
+                    title: "Offical Guide",
+                    description: "This is an official guide.",
+                    link: "https://officialguide.com",
+                },
+                {
+                    id: 2,
+                    title: "Unoffical Guide",
+                    description: "This is an unofficial guide.",
+                    link: "https://unofficialguide.com",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: "Roboto", sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
